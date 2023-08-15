54b71660c20d8a85e651c4645db97fd802b79339
caraka15 <carakawidi07@gmail.com>
2023-08-15 21:51:20 +0700

    fix bug

--- index.php
+++ index.php
@@ -34,8 +34,7 @@
     <!-- partial:index.partial.html -->
     <?php include 'component/includes/header.php' ?>
 
-    <a class="banner" href="token.php"
-        target="_blank">
+    <a class="banner" href="token.php" target="_blank">
         <p>Klik di sini untuk melihat detail</p>
     </a>
 
@@ -68,32 +67,18 @@
         </div>
     </div>
 
-    <!-- Pop-Up Container -->
-    <div class="popup-container" id="popupContainer">
-        <div class="popup-content">
-            <iframe
-                src="https://embed.ipfscdn.io/ipfs/bafybeigtqeyfmqkfbdu7ubjlwhtqkdqckvee7waks4uwhmzdfvpfaqzdwm/erc20.html?contract=0x11Cfefe281cc15439ceF86a76d3Db5769cb1C53F&chain=%7B%22name%22%3A%22Linea+Testnet%22%2C%22chain%22%3A%22ETH%22%2C%22rpc%22%3A%5B%22https%3A%2F%2Flinea-testnet.rpc.thirdweb.com%2F%24%7BTHIRDWEB_API_KEY%7D%22%5D%2C%22nativeCurrency%22%3A%7B%22name%22%3A%22Linea+Ether%22%2C%22symbol%22%3A%22ETH%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22linea-testnet%22%2C%22chainId%22%3A59140%2C%22testnet%22%3Atrue%2C%22slug%22%3A%22linea-testnet%22%2C%22icon%22%3A%7B%22url%22%3A%22ipfs%3A%2F%2FQmURjritnHL7a8TwZgsFwp3f272DJmG5paaPtWDZ98QZwH%22%2C%22width%22%3A97%2C%22height%22%3A102%2C%22format%22%3A%22svg%22%7D%7D&clientId=884b00a0807ee54b59a172b6c49a9f52&primaryColor=orange"
-                width="600px"
-                height="600px"
-                style="max-width:100%;"
-                frameborder="0"
-            ></iframe>
-            <button id="closePopup">Tutup</button>
-        </div>
-    </div>
-
     <script>
-        const openPopupButton = document.getElementById('openPopup');
-        const closePopupButton = document.getElementById('closePopup');
-        const popupContainer = document.getElementById('popupContainer');
+    const openPopupButton = document.getElementById('openPopup');
+    const closePopupButton = document.getElementById('closePopup');
+    const popupContainer = document.getElementById('popupContainer');
 
-        openPopupButton.addEventListener('click', () => {
-            popupContainer.style.display = 'flex';
-        });
+    openPopupButton.addEventListener('click', () => {
+        popupContainer.style.display = 'flex';
+    });
 
-        closePopupButton.addEventListener('click', () => {
-            popupContainer.style.display = 'none';
-        });
+    closePopupButton.addEventListener('click', () => {
+        popupContainer.style.display = 'none';
+    });
     </script>
 
     <?php include 'component/includes/footer.php' ?>
